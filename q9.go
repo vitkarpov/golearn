e8b9803ec421515a0b89cc6e2ed5514266809266
vitkarpov <viktor.s.karpov@gmail.com>
2015-07-09 13:14:41 +0300

    adds desciption of q9

---
+++ q9.go
@@ -0,0 +1,13 @@
+/**
+ * Create a simple stack which can hold a fixed number of ints.
+ * It does not have to grow beyond this limit.
+ * Define push – put something on the stack – and pop – retrieve something from the stack – functions.
+ * The stack should be a LIFO (last in, first out) stack.
+ *
+ * Bonus: write a String method which converts the stach to a string representation.
+ * This way you can print the stack using: `fmt.Printf("My stach %v'n", stack)`.
+ * A stack could be represented: [0:m] [1:l] [2:k]
+ */
+
+package golearn
+
