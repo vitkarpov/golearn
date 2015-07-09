ece34ec9bd37ff220927c0df1b35227c7176befe
vitkarpov <viktor.s.karpov@gmail.com>
2015-07-09 13:09:47 +0300

    adds question's description of q7

--- q7.go
+++ q7.go
@@ -1,3 +1,10 @@
+/**
+ * Write a function that returns its (two) parameters
+ * in the right, numerica (ascending) order:
+ * f(7,2) -> 2,7
+ * f(2,7) -> 2,7
+ */
+
 package golearn
 
 func LessFirst(i, j int) (int, int) {
